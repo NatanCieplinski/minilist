8857aa479d5f62081100ae9135a4d9766b842551
NatanCieplinski <natan.cieplinski.developer@gmail.com>
2020-05-23 01:02:48 +0200

    Lint script fix & dependencies update

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,8 @@
 module.exports = {
    root: true,
-   parser: 'babel-eslint',
+   parser: 'vue-eslint-parser',
    parserOptions: {
+      parser: 'babel-eslint',
       sourceType: 'module'
    },
    env: {
@@ -11,9 +12,21 @@ module.exports = {
    globals: {
       __static: true
    },
-   plugins: ['html'],
+   plugins: ['eslint-plugin-vue'],
    rules: {
       // allow debugger during development
-      'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
-   }
+      'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+      indent: ['error', 3],
+      'vue/component-tags-order': [
+         'error',
+         {
+            order: ['template', 'script', 'style']
+         }
+      ]
+   },
+   extends: [
+      // add more generic rulesets here, such as:
+      // 'eslint:recommended',
+      'plugin:vue/vue3-recommended'
+   ]
 }
